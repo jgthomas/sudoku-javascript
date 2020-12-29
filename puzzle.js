009db2a6d10fb9de4f4d149e18adbc511cded6be
James Thomas <codeactor@protonmail.com>
2020-12-29 17:49:39 +0000

    First code for solver

--- puzzle.js
+++ puzzle.js
@@ -37,6 +37,73 @@ class Puzzle {
 
     return puzzleRow;
   }
+
+  solve() {
+    let row = 0;
+    let col = 0;
+    let movingForward = true;
+
+    while (row * col < this.totalSquares) {
+      let currentSquare = this.puzzle[row][col];
+
+      if (currentSquare.presentFromStart) {
+        if (movingForward) {
+          col++;
+        } else {
+          col--;
+        }
+      }
+    }
+  }
+
+  fillSquare(row, col) {
+    const currentNumber = this.puzzle[row][col].number;
+    let firstTry = currentNumber;
+
+    if (currentNumber === 0) {
+      firstTry = 1;
+    }
+
+    for (let num = firstTry; num <= this.maxNumber; num++) {
+      if (this.numberAllowed(row, col, num)) {
+        puzzle[row][col].number = num;
+        return true;
+      }
+    }
+
+    this.puzzle[row][col].number = 0;
+    return false;
+  }
+
+  numberAllowed(row, col, num) {
+    if (
+      this.notInRow(row, num) &&
+      this.notInCol(col, num) &&
+      this.notInBox(row, col, num)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  notInRow(row, num) {
+    return !this.puzzle[row].includes(num);
+  }
+
+  notInCol(col, num) {
+    for (row in this.puzzle) {
+      if (row[col] === num) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  notInBox(row, col, num) {
+    return false;
+  }
 }
 
 module.exports = Puzzle;
