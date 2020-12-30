93107a9f2cc4ee1bb0ecf1dbecb978466917cba4
James Thomas <codeactor@protonmail.com>
2020-12-30 15:54:04 +0000

    Test for puzzle solution

--- puzzle.test.js
+++ puzzle.test.js
@@ -1,4 +1,4 @@
-const { test, expect } = require("@jest/globals");
+const { test, expect, beforeAll } = require("@jest/globals");
 const Puzzle = require("./puzzle");
 
 const samplePuzzleSize = 9;
@@ -15,8 +15,21 @@ const samplePuzzle = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-test("Backtrack position", () => {
+const solvedPuzzle = [
+  [1, 5, 6, 2, 4, 7, 3, 8, 9],
+  [4, 3, 7, 5, 8, 9, 6, 1, 2],
+  [2, 8, 9, 1, 3, 6, 7, 4, 5],
+  [3, 1, 2, 4, 5, 8, 9, 6, 7],
+  [5, 6, 4, 7, 9, 1, 2, 3, 8],
+  [7, 9, 8, 3, 6, 2, 1, 5, 4],
+  [6, 2, 1, 8, 7, 4, 5, 9, 3],
+  [8, 7, 3, 9, 1, 5, 4, 2, 6],
+  [9, 4, 5, 6, 2, 3, 8, 7, 1],
+];
+
+test("Solver works for basic valid puzzle", () => {
   const puzzle = new Puzzle(samplePuzzleSize, samplePuzzle);
-  const newPostion = puzzle.backtrackPosition(0, 1);
-  expect(newPostion).toEqual([0, 0]);
+  puzzle.solve();
+  const solution = puzzle.toIntArray();
+  expect(solution).toEqual(solvedPuzzle);
 });
