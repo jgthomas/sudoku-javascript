d4bab408606f01542c7497d64d42f55ace5c8928
James Thomas <codeactor@protonmail.com>
2020-12-30 16:05:01 +0000

    Move test data to own file

--- puzzle.test.js
+++ puzzle.test.js
@@ -1,35 +1,17 @@
 const { test, expect, beforeAll } = require("@jest/globals");
 const Puzzle = require("./puzzle");
-
-const samplePuzzleSize = 9;
-
-const samplePuzzle = [
-  [1, 0, 0, 2, 0, 0, 3, 0, 0],
-  [4, 0, 0, 5, 0, 0, 6, 0, 0],
-  [2, 0, 0, 1, 0, 0, 7, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-const solvedPuzzle = [
-  [1, 5, 6, 2, 4, 7, 3, 8, 9],
-  [4, 3, 7, 5, 8, 9, 6, 1, 2],
-  [2, 8, 9, 1, 3, 6, 7, 4, 5],
-  [3, 1, 2, 4, 5, 8, 9, 6, 7],
-  [5, 6, 4, 7, 9, 1, 2, 3, 8],
-  [7, 9, 8, 3, 6, 2, 1, 5, 4],
-  [6, 2, 1, 8, 7, 4, 5, 9, 3],
-  [8, 7, 3, 9, 1, 5, 4, 2, 6],
-  [9, 4, 5, 6, 2, 3, 8, 7, 1],
-];
+const { samplePuzzleSize, samplePuzzle, solvedPuzzle } = require("./test-data");
 
 test("Solver works for basic valid puzzle", () => {
   const puzzle = new Puzzle(samplePuzzleSize, samplePuzzle);
   puzzle.solve();
   const solution = puzzle.toIntArray();
   expect(solution).toEqual(solvedPuzzle);
+});
+
+test("Solver selects first potential value for the first empty position", () => {
+  const puzzle = new Puzzle(samplePuzzleSize, samplePuzzle);
+  const square = puzzle.puzzle[0][1];
+  const number = puzzle.numberForPosition(square);
+  expect(number).toBe(5);
 });
